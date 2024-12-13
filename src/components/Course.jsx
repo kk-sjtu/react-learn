f074412c1ae07d354c67a4d6fb33c60cd1069e24
Your Name <you@example.com>
2024-12-13 16:57:40 +0800

    s

---
+++ src/components/Course.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Course = ({ course}) => {
+    const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+    return (
+        <div>
+        <h1>{course.name}</h1>
+        {course.parts.map(part =>
+            <p key={part.id}>
+            {part.name} {part.exercises}
+            </p> 
+        )}
+        {<p><strong>total of {total} exercises</strong></p>}
+    
+        </div>
+    )
+    }
+
+export default Course
